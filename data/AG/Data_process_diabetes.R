eb7285a66dea5a86fad41e6f1cd967b759c52a35
Jialiang Mao <jialiangmao@JialiangdeMacBook-Pro-2.local>
2020-08-07 01:40:19 -0400

    add original AG data

---
+++ data/AG/Data_process_diabetes.R
@@ -0,0 +1,60 @@
+
+rm(list = ls())
+setwd("~/data/AG")
+
+library(data.table)
+library(phyloseq)
+library(ape)
+library(dplyr)
+
+AG_tree = read_tree_greengenes("97_otus.tree")        #-import the phylogenetric tree
+otu = fread('ag_fecal_from_biom.txt', header = TRUE)  #-import the otu table
+
+
+otu_matrix = do.call(rbind, otu)
+otu_matrix = apply(otu_matrix,1, as.numeric)
+row.names(otu_matrix) = otu_matrix[, "OTUID"]
+otu_neat = otu_matrix[, -1]
+rm(otu_matrix)
+
+
+#################################################################################################
+####----choose subset of samples
+
+ag_fecal = fread("ag_fecal.txt")
+
+ag_fecal = ag_fecal[ag_fecal$DIABETES == "Diagnosed by a medical professional (doctor, physician assistant)"]
+
+otu_neat = otu_neat[, intersect(colnames(otu_neat), ag_fecal$SampleID) ]
+
+#################################################################################################
+####----choose subset of otus based on some criterion
+
+otu_rowsum = apply(otu_neat, 1, sum)
+otu_colsum = apply(otu_neat, 2, sum)
+
+num_otu_top = 75  ####----number of top otus to be used in the analysis
+num_min_sample = 500
+
+cutoff = sort(otu_rowsum, decreasing = TRUE)[num_otu_top]
+
+otu_top = otu_neat[otu_rowsum >= cutoff, ]
+otu_top = otu_top[, apply(otu_top, 2, sum) >= num_min_sample]
+
+ag_fecal = ag_fecal[ag_fecal$SampleID %in% colnames(otu_top), ]
+
+#################################################################################################
+####---create a trimmed otu tree with only the otus selected above
+
+diff = setdiff(AG_tree$tip.label, row.names(otu_top))
+tree = drop.tip(AG_tree, tip=diff, trim.internal = TRUE, subtree = FALSE, root.edge = 0)
+
+#################################################################################################
+####----remove the ununsed variables
+
+rm(otu, otu_neat, AG_tree, cutoff, diff, num_otu_top, otu_rowsum, otu_colsum)
+
+#################################################################################################
+####----save the data
+
+save(otu_top, tree, ag_fecal, file="./AG_diabetes.RData")
--- data/AG/README.txt
+++ data/AG/README.txt
@@ -1,10 +1,16 @@
-This folder contains the American Gut data for the case studies. The two datasets are preprocessed based on the July 29, 2016 version of the fecal data from the American Gut Project. Each dataset contains an OTU table and a phylogenetic tree. The OTU tables contain counts of the top 75 OTUs selected by the total counts of the OTUs in all samples. 
+This folder contains the American Gut data for the case studies. The two datasets "AG_ibd.RData" and "AG_diabetes.RData" are preprocessed based on the July 29, 2016 version of the fecal data from the American Gut Project. Each dataset contains an OTU table and a phylogenetic tree. The OTU tables contain counts of the top 75 OTUs selected by the total counts of the OTUs in all samples. 
 
 AG_ibd.RData is the IBD dataset used in Section 4.1. The dataset has 189 samples from IBD patients in the study.
 
 AG_diabetes.RData is the Diabetes dataset used in Section 4.2. The dataset has 106 samples from diabetes patients in the study.
 
-"97_otu_taxonomy.txt" contains a taxa table for the AG dataset.
+These two datasets are obtained by directly running the preprocessing code in "Data_process_ibd.R" and "Data_process_diabetes.R", respectively. The code preprocess the original AG data by selecting subset of samples and OTUs. Details of the preprocessing procedure is given in the paper.
+
+The original July 29, 2016 version of the fecal dataset in the American Gut Project contains 4 parts:
+1. "97_otu_taxonomy.txt" contains a taxa table of all OTUs in the study;
+2. "97_otus.tree" contains a rooted full binary phylogenetic tree of these OTUs;
+3. "ag_fecal_from_biom.txt" contains the entire OTU table of all samples. This table is directly obtained based on the sequencing reads.
+4. "ag_fecal.txt" is the metadata of the study.
 
 
 
